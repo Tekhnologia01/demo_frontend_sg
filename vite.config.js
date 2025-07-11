8e8e005d08185f8fb5d596313a23808cbaa92cab
Tekhnologia01 <git@tekhnologiaindia.com>
2025-07-11 12:20:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+export default {
   server: {
-    open: true
+    host: '0.0.0.0',
+    port: 4000
   }
-})
+}
